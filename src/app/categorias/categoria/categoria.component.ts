a2c1bf664244269c8e73da8492498dd20dd2f6ac
Leonardo Ramos <leonardosr793@gmail.com>
2026-07-04 21:48:08 -0300

    Implementação de Reactive Forms e validações no formulário de cadastro de categorias

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
   standalone: false,
+
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
+
+  constructor(){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    })
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched || false;
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      console.log('valores digitados: ', this.camposForm.value)
+    }
+      
+  }
 
 }
